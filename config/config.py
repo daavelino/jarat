09ebe06b9a5ef0046d2e7785970f539f700040f9
Daniel Avelino <daavelino@gmail.com>
2023-10-31 15:16:43 +0100

    Update config.py

--- config/config.py
+++ config/config.py
@@ -6,4 +6,4 @@ SERVER = 'https://...'
 ACCESS_TOKEN = None
 ROOT_OBJECT_SCHEME=1
 ATTRIBUTE_DEEP=1
-RESULTS_PER_PAGE = 200000
+RESULTS_PER_PAGE=200000 # If lower than 'totalFilterCounter' in the returned results, pagination applies.
